4e2c683b00e279e4e6da91d5678a5243ec2791f9
YuraLavrinov <123143.hF>
2021-05-07 02:47:13 +0300

    clean up code

--- src/main/java/mazeRunner/RandomLvl.java
+++ src/main/java/mazeRunner/RandomLvl.java
@@ -70,7 +70,6 @@ public class RandomLvl {
             }
             randomArray[10][10] = BOSS.getBoss();
         }
-        System.out.println(EXIT.getExit());
         return randomArray;
     }
 }
